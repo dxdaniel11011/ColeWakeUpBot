8aa0dc28b7d033f751b39c250aef47569db353bf
Rip Etika Etika <>
2021-05-19 00:34:27 +0000

    cole changed the spamcole command to almost support two digit numbers

--- main.py
+++ main.py
@@ -13,10 +13,13 @@ async def on_message(message):
 
     if message.content.startswith('$spamcole('):
       if message.content[11] in [1,2,3,4,5,6,7,8,9,0]:
-          for i in range(int(message.content[10:11])):
+          num = message.content[11] + message.content[12]
+          for i in range(int(num)):
+            await message.channel.send("q")
             await message.channel.send("<@145631773328539649> WAKE THE FUCK UP")
       else:
           for i in range(int(message.content[10])):
+            await message.channel.send("b")
             await message.channel.send("<@145631773328539649> WAKE THE FUCK UP")
       
 
