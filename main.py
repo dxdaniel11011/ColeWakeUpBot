6b0bf8110a5f49c0542547e5d49f603ad64a042e
Rip Etika Etika <>
2021-05-19 00:45:16 +0000

    INFINITE NUMBERS BABY!!!!

--- main.py
+++ main.py
@@ -11,16 +11,15 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$spamcole('):
-      if message.content[11] in [1,2,3,4,5,6,7,8,9,0]:
-          num = message.content[11] + message.content[12]
-          for i in range(int(num)):
-            await message.channel.send("q")
-            await message.channel.send("<@145631773328539649> WAKE THE FUCK UP")
-      else:
-          for i in range(int(message.content[10])):
-            await message.channel.send("b")
-            await message.channel.send("<@145631773328539649> WAKE THE FUCK UP")
+    # if message.content.startswith('$spamcole('):
+    #   if message.content[11] in [1,2,3,4,5,6,7,8,9,0]:
+    #       num = message.content[10:len(message.content)-1]
+    #       for i in range(int(num)):
+    #         await message.channel.send("q")
+    #         await message.channel.send("<@145631773328539649> WAKE THE FUCK UP")
+    #   else:
+    for i in range(int(message.content[10:len(message.content)-1])):
+      await message.channel.send("<@145631773328539649> WAKE THE FUCK UP")
       
 
 TOKEN = os.getenv('TOKEN')
